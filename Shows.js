f4210e823f575d73616f7e6c206ad0f77e44a1af
Juan Martín Diez <diez.juan.martin12@gmail.com>
2023-07-20 16:52:16 -0300

    Segunda PreEntrega

---
+++ Shows.js
@@ -0,0 +1,78 @@
+/*const shows =
+    [
+        {
+            id: 1,
+            artista: "Taylor Swift - The Eras Tour",
+            tipo: "Internacional",
+            fechas: "9, 10 y 11 de noviembre",
+            lugar: "Estadio River Plate (Mâs Monumental)",
+            img: "./assets/pages/TaylorSwift.html",
+            cantidad: 5
+        },
+
+        {
+            id: 2,
+            artista: "Kendrick Lamar - The Big Steppers Tour",
+            tipo: "Internacional",
+            fechas: "28 de agosto",
+            lugar: "Rogers Arena",
+            img: "./assets/pages/KendrickLamar.html",
+            cantidad: 5
+        },
+
+        {
+            id: 3,
+            artista: "JOJI - Smithereens tour",
+            tipo: "Internacional",
+            fechas: "6 de septiembre",
+            lugar: "Wamu Theater",
+            img: "./assets/img/joji_card.jpg",
+            cantidad: 5
+        },
+
+        {
+            id: 4,
+            artista: "Tyler The Creator - Call Me If You Get Tour",
+            tipo: "Internacional",
+            fechas: "2 de abril",
+            lugar: "Golden 1 Center",
+            img: "./assets/img/tyler_the_creator_card.jpg",
+            cantidad: 5
+        },
+    ];
+
+const showNoDisponible =
+    [
+        {
+            id: 1,
+            artista: "Kanye West - Saint Pablo Tour",
+            tipo: "Internacional",
+            fechas: "27 de octubre",
+            lugar: "Mercedes Benz Stadium",
+            disponibilidad: "SHOW CANCELADO",
+            img: "./assets/img/kanye_west_card.jpg",
+            cantidad: 0
+        },
+
+        {
+            id: 2,
+            artista: "TheWeeknd - After Hours Till Dawn Tour",
+            tipo: "Internacional",
+            fechas: "30 de julio",
+            lugar: "Levi´s &copy Stadium",
+            disponibilidad: "ENTRADAS AGOTADAS",
+            img: "./assets/img/weeknd_card.jpeg",
+            cantidad: 0
+        },
+
+        {
+            id: 3,
+            artista: "Childish Gambino - This Is America World Tour",
+            tipo: "Internacional",
+            fechas: "24 y 25 de marzo",
+            lugar: "O2 Arena",
+            disponibilidad: "ENTRADAS AGOTADAS",
+            img: "./assets/img/gambino_card.jpg",
+            cantidad: 0
+        },
+    ];*/
--- main.js
+++ main.js
@@ -111,3 +111,38 @@ const comprarEntradas = () => {
 };
 
 comprarEntradas();
+
+/*
+
+let producto = { id: 1, nombre: "Arroz", precio: 125 };
+
+let contenedor = document.createElement("div")
+
+contenedor.innerHTML = `
+<h3>ID: ${producto.id}</h3>
+<p>Producto: ${producto.nombre}</p>
+<b>$ ${producto.precio}</b>
+`
+
+document.body.appendChild(contenedor)
+
+const cardsShows = () => {
+    const contenedor = document.getElementsByClassName("cardContainer")
+
+    shows.forEach(show => {
+        const div = document.createElement("div")
+        div.className = "card"
+        div.innerHTML = `
+        <a href="./assets/pages/TaylorSwift.html"><img src=${show.img}
+                        class="indexCards" alt="Taylor Swift - Eras Tour"></a>
+                <h1>${show.artista}</h1>
+                <h2>${show.fechas}</h2>
+                <h3>${show.lugar}</h3>
+`
+        contenedor.appendChild(div)
+    })
+}
+
+cardsShows()
+
+*/
